3985c6968bbcea7a53245950c06346c2d17b12c0
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2019-01-04 09:46:44 +0100

    axios path

--- static/app.js
+++ static/app.js
@@ -4,6 +4,7 @@ requirejs.config({
         common: '../../common/lib',
         app: '../app',
         webix: '//cdn.webix.com/4.4/webix_debug',
+        axios: '//unpkg.com/axios/dist/axios.min'
     },
     shim: {
         webix: {
