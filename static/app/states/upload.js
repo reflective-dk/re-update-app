81e516b8880fb92a6c653612c6005909c8353b03
Kenneth <kenneth@reflective.dk>
2019-12-05 11:11:25 +0000

    changed layout

--- static/app/states/upload.js
+++ static/app/states/upload.js
@@ -4,37 +4,28 @@ function (webix, stateRouter) {
   return {
     name: 'upload',
     template: {
-      $ui: {
-        rows:[
-          {},
-          {cols: [
-            {},
-            { type: "form", rows: [
-              {
-                id: "uploader",
-                view: "uploader",
-                value: 'Tryk for udvælgelse af fil, eller træk og slip filen her.',
-                link: "list",
-                upload: "/app/upload/file",
-                css: {'color': 'white'}
-              },
-              {
-                id: "list",
-                view: "list",
-                type: "uploader",
-                autoheight: true,
-                autowidth: true,
-                borderless: true,
-              }]
-              },
-            {}
-          ]},
-          {}
-        ]
+      $ui: { 
+        type: "form",
+        rows: [{
+          id: "uploader",
+          view: "uploader",
+          value: 'Tryk for udvælgelse af fil, eller træk og slip filen her.',
+          link: "list",
+          upload: "/app/upload/file",
+          css: {'color': 'white'}
+        },
+        {
+          id: "list",
+          view: "list",
+          type: "uploader",
+          autoheight: true,
+          autowidth: true,
+          borderless: true,
+        }]
       },
       $oninit: function (view, scope) {
         $$("uploader").attachEvent("onFileUpload", function(item, response) {
-          item.name = response.filename;
+          item.name = response.filename + ', ' + response.hash;
         });
      }
     },
