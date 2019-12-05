3d0066db24cd7d253d2b494000995dd326cd5ecd
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2019-12-05 11:37:49 +0100

    clean-up

--- static/app/states/upload.js
+++ static/app/states/upload.js
@@ -3,37 +3,41 @@ function (webix, stateRouter) {
 
   return {
     name: 'upload',
+    route: '/upload-files',
     template: {
-      $ui: { 
-        type: "form",
-        rows: [{
-          id: "uploader",
-          view: "uploader",
-          value: 'Tryk for udvælgelse af fil, eller træk og slip filen her.',
-          link: "list",
-          upload: "/app/upload/file",
-          css: {'color': 'white'}
-        },
-        {
-          id: "list",
-          view: "list",
-          type: "uploader",
-          autoheight: true,
-          autowidth: true,
-          borderless: true,
-        }]
+      $ui: {
+        rows:[
+          {},
+          {cols: [
+            {},
+            { type: "form", rows: [
+              {
+                id: "uploader",
+                view: "uploader",
+                height: 200,
+                width: 500,
+                value: 'Tryk for udvælgelse af fil, eller træk og slip filen her.',
+                link: "list",
+                upload: "/app/upload/file"
+              },
+              {
+                id: "list",
+                view: "list",
+                type: "uploader",
+                autoheight: true,
+                borderless: true
+              }]
+              },
+            {}
+          ]},
+          {}
+        ]
       },
       $oninit: function (view, scope) {
         $$("uploader").attachEvent("onFileUpload", function(item, response) {
           item.name = response.filename + ', ' + response.hash;
         });
      }
-    },
-    resolve: function(data, parameters, cb) {
-      cb();
-  	},
-    activate: function(context) {
-  	},
-    route: '/upload',
+    }
   };
 });
